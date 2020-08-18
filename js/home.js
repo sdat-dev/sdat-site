c9601cb94de591c43c3c174ce43bf2515327f0e7
sdat-dev <sdat@albany.eduq>
2020-08-18 16:42:13 -0400

    updated site according to latest template

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-let requestURL = "data/home.json";
+let requestURL = "data/home.json"
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
@@ -11,19 +11,49 @@ request.onload = function(){
     //condition for checking if browser is Internet Explorer
     let webelements =  ((false || !!document.documentMode))? JSON.parse(webelementsjson): webelementsjson;
     let logostart = true;
+    let pageheaders = [];
     for(let i = 0; i < webelements.length; i++)
     {
         let element = webelements[i]; 
-        if(element.type == 'header')
+        let type = element.type.toLowerCase(); 
+        if(type == 'ph')
+        {
+            pageheaders.push(element);
+        }
+        else if(type == 'ch')
         {
             let header = document.getElementsByClassName("content-header")[0];
             header.innerHTML = element.content.toUpperCase();
         }
-        else if(element.type == 'p')
+        else if(type == 'p')
         {
             content += '<p>' + element.content + '</p>';
         }
-        else if(element.type == 'a' && element.logo != '')
+        else if(type == 'img')
+        {
+            content += '<img src="assets/images/'+ element.content + '" alt="" style="width: 100%;">';
+        }
+        else if(type == 'iframe')
+        {
+            content += '<iframe '+ element.content +'></iframe>';
+        }
+        else if(type == 'ul')
+        { 
+            content += '<ul class="sub-list ' + element.content +'">';
+        }
+        else if(type == 'li')
+        {
+            content += '<li>'+ element.content +'</li>';
+        }
+        else if(type == '/ul')
+        {
+            content += '</ul>';
+        }
+        else if(type == 'a' && !element.hasOwnProperty("logo"))
+        {
+            content +='<a href = "'+ element.source +'">'+ element.content + '</a>';
+        }
+        else if(type == 'a' && element.logo != '')
         {
             if(logostart == true)
             {
@@ -31,9 +61,9 @@ request.onload = function(){
                 logostart = false;
             }
             content +='<div class = "col-xl-4 col-lg-6 col-md-12">'+
-                        '<a href = "'+ element.source +'">'+
+                        '<a target = "_blank" href = "'+ element.source +'">'+
                             '<div class = "home-logo-container">' +
-                                '<img class = "home-logo" src = "assets/images/home/' + element.logo+ '">'+
+                                '<img class = "home-logo" src = "assets/images/' + element.logo+ '">'+
                                 '<p>'+element.content+'</p>' +
                             '</div>'+
                         '</a>'+
@@ -43,9 +73,46 @@ request.onload = function(){
             }
         }
     }
-
+    addheader(pageheaders);
     let contentElement = document.createElement('div');
     contentElement.classList.add('content');
     contentElement.innerHTML = content.trim();
     maincontentContainer.appendChild(contentElement);
+    addfooter();
 }
+
+let addheader =  function (headers){
+    let header = document.getElementById("page-header");
+    let content ="";
+    let image = "";
+    let header1 = "";
+    let header2 = "";
+
+    content += '<div class="carousel slide carousel-fade pointer-event" data-ride="carousel">'+
+                    '<div class="carousel-inner">';
+    for(var i =0 ; i < headers.length; i++)
+    {
+        image = typeof headers[i].image != 'undefined' && headers[i].image != ''? headers[i].image : image;
+        header1 =  typeof headers[i].content != 'undefined' && headers[i].content != ''? headers[i].content : header1;
+        header2 =  typeof headers[i].subcontent != 'undefined' && headers[i].subcontent != ''? headers[i].subcontent : header2;
+        let source = 'assets/images/' + headers[i].source != ''? headers[i].source+'/' : '';
+        if(i == 0)
+        {
+            content += '<div class="carousel-item active">';
+        }
+        else
+        {
+            content += '<div class="carousel-item">';
+        }
+        content +=  '<img src="'+ source + image +'" class="d-block w-100" alt="...">'+
+                    '<div id = "landing-page-text-wrapper">'+
+                        '<h1>'+ header1 +'</h1>'+ 
+                        '<p>' + header2 + '</p>'        
+                    '</div>'+
+                '</div>';
+    }
+    content +=  '</div></div>';
+    header.innerHTML = content;
+}
+
+$('.carousel').carousel({pause: false});
